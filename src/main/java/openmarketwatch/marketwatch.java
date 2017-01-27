67e41c64bc9eca522010fcc334ecb7e4fac59f01
jasonsouryamath <jasonsouryamath@yahoo.com>
2017-01-27 01:06:04 -0800

    Update marketwatch.java

--- src/main/java/openmarketwatch/marketwatch.java
+++ src/main/java/openmarketwatch/marketwatch.java
@@ -14,11 +14,13 @@ import org.testng.annotations.Test;
 
 public  class marketwatch {
 // public static void is used 
+//@Test
+WebDriver driver;
 @Test
 public void TestMarketWatch() throws InterruptedException {
-WebDriver driver;
+//WebDriver driver;
 // create new firefox webdriver isntance by setting the properties using the code below 
-System.setProperty("webdriver.firefox.bin","/Applications/Firefox.app/Contents/MacOS/firefox-bin/");
+System.setProperty("webdriver.firefox.bin","/Users/jasonsouryamath/Desktop/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin");
 driver = new FirefoxDriver();
 driver.get("http://www.marketwatch.com/");
 
