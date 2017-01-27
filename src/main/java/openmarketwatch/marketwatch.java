b49a60765bcf07f860ea3230f8a0f6e9b200406f
jasonsouryamath <jasonsouryamath@yahoo.com>
2017-01-27 09:23:24 -0800

    Update marketwatch.java

--- src/main/java/openmarketwatch/marketwatch.java
+++ src/main/java/openmarketwatch/marketwatch.java
@@ -20,8 +20,10 @@ WebDriver driver;
 public void TestMarketWatch() throws InterruptedException {
 //WebDriver driver;
 // create new firefox webdriver isntance by setting the properties using the code below 
-System.setProperty("webdriver.firefox.bin","/Users/jasonsouryamath/Desktop/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin");
-driver = new FirefoxDriver();
+//System.setProperty("webdriver.firefox.bin","/Users/jasonsouryamath/Desktop/FirefoxDeveloperEdition.app/Contents/MacOS/firefox-bin");
+//driver = new FirefoxDriver();
+ System.setProperty("webdriver.gecko.driver", "C:\\Users\\jsouryamath\\Desktop\\geckodriver.exe");
+ driver =new FirefoxDriver();
 driver.get("http://www.marketwatch.com/");
 
 WebElement stock = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".btn--search")));
