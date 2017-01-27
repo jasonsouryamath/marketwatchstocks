aeca7c9f015cf4e8566b30bf9ca4ecadb4a438fe
jasonsouryamath <jasonsouryamath@yahoo.com>
2017-01-26 22:18:50 -0800

    Update marketwatch.java

--- src/main/java/openmarketwatch/marketwatch.java
+++ src/main/java/openmarketwatch/marketwatch.java
@@ -18,7 +18,7 @@ public  class marketwatch {
 public static void TestMarketWatch() throws InterruptedException {
 WebDriver driver;
 // create new firefox webdriver isntance by setting the properties using the code below 
-//System.setProperty("webdriver.firefox.bin","/Users/jasonsouryamath/Desktop/Firefox.app/Contents");
+System.setProperty("webdriver.firefox.bin");
 driver = new FirefoxDriver();
 driver.get("http://www.marketwatch.com/");
 
